17357b922d0bcb8da59e4f3ea26f72441807a3c4
Jhonatan Vergara <j.vergaracajas@gmail.com>
2024-04-29 09:46:27 -0400

    fix htacces

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -50,7 +50,7 @@
             </div>
             <div class="form-group">
                 <label class="required" for="email">{{ trans('cruds.user.fields.email') }}</label>
-                <input class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" type="email" name="email" id="email" value="{{ old('email'), '' }}" required>
+                <input class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" type="email" name="email" id="email" value="{{ old('email', '' )}}" required>
                 @if($errors->has('email'))
                     <div class="invalid-feedback">
                         {{ $errors->first('email') }}
