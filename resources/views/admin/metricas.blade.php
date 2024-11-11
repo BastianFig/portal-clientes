1b32fa8f8465204971f0084149d4e13cd2d24172
Jhonatan Vergara <j.vergaracajas@gmail.com>
2024-11-11 14:06:24 -0300

    fx

--- resources/views/admin/metricas.blade.php
+++ resources/views/admin/metricas.blade.php
@@ -1,19 +1,17 @@
 @extends('layouts.admin')
 @section('content')
-    
     @foreach ($proyectos as $proyecto)
         <div class="card">
             <div class="card-head">
-                <h2>{{$proyecto->vendedor_nombre }}</h2>
+                <h2>{{ $proyecto->vendedor_nombre }}</h2>
             </div>
             <div class="card-body">
-                <p>{{ $proyecto->fase }}</p>
-                <p>{{ $proyecto->total_proyectos }}</p>
+                <p><strong>Fases:</strong> {{ $proyecto->fases }}</p>
+                <p><strong>Total Proyectos:</strong> {{ $proyecto->total_proyectos }}</p>
             </div>
             <div class="card-footer">
-
+                <!-- Aquí puedes agregar algún botón o enlace si es necesario -->
             </div>
         </div>
     @endforeach
-        
 @endsection
