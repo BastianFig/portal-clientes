21e8f3debe62642a2fd496c5215b3d1c249fd43a
Jhonatan Vergara <j.vergaracajas@gmail.com>
2024-11-11 13:48:44 -0300

    fx4 p1 metricas

--- resources/views/admin/metricas.blade.php
+++ resources/views/admin/metricas.blade.php
@@ -1,14 +1,16 @@
 @extends('layouts.admin')
-<div class="card">
-    <div class="card-head">
+@section('content')
+    <div class="card">
+        <div class="card-head">
 
-    </div>
-    <div class="card-body">
-        @foreach ($proyectos as $proyecto)
-            <p>{{$proyecto->id}}</p>
-        @endforeach
-    </div>
-    <div class="card-footer">
+        </div>
+        <div class="card-body">
+            @foreach ($proyectos as $proyecto)
+                <p>{{$proyecto->id}}</p>
+            @endforeach
+        </div>
+        <div class="card-footer">
 
+        </div>
     </div>
-</div>
+@endsection
