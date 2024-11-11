5970b7817dad57f5751639dd08120ab2c49e9b2d
Jhonatan Vergara <j.vergaracajas@gmail.com>
2024-11-11 15:14:09 -0300

    fx

--- resources/views/admin/metricas.blade.php
+++ resources/views/admin/metricas.blade.php
@@ -55,37 +55,52 @@
                                             <p>{{ $proyecto->total_fase }} proyectos</p>
                                         @elseif($proyecto->fase == 'Fase Propuesta Comercial')
                                             <p><strong>Fase Propuesta Comercial</strong></p>
-                                            <div class="progress-bar" style="width: {{ $porcentaje }}%">
+                                            <div class="d-flex">
+                                                <div class="progress-bar mr-2" style="width: {{ $porcentaje }}%">
+                                                </div>
                                                 <span>{{ number_format($porcentaje, 0) }}%</span>
                                             </div>
                                             <p>{{ $proyecto->total_fase }} proyectos</p>
                                         @elseif($proyecto->fase == 'Fase Contable')
                                             <p><strong>Fase Contable</strong></p>
+                                            <div class="d-flex">
+                                                <div class="progress-bar mr-2" style="width: {{ $porcentaje }}%">
+                                                </div>
+                                                <span>{{ number_format($porcentaje, 0) }}%</span>
+                                            </div>
                                             <div class="progress-bar" style="width: {{ $porcentaje }}%">
                                                 <span>{{ number_format($porcentaje, 0) }}%</span>
                                             </div>
                                             <p>{{ $proyecto->total_fase }} proyectos</p>
                                         @elseif($proyecto->fase == 'Fase Acuerdo Comercial')
                                             <p><strong>Fase Acuerdo Comercial</strong></p>
-                                            <div class="progress-bar" style="width: {{ $porcentaje }}%">
+                                            <div class="d-flex">
+                                                <div class="progress-bar mr-2" style="width: {{ $porcentaje }}%">
+                                                </div>
                                                 <span>{{ number_format($porcentaje, 0) }}%</span>
                                             </div>
                                             <p>{{ $proyecto->total_fase }} proyectos</p>
                                         @elseif($proyecto->fase == 'Fase Fabricacion')
                                             <p><strong>Fase Fabricación</strong></p>
-                                            <div class="progress-bar" style="width: {{ $porcentaje }}%">
+                                            <div class="d-flex">
+                                                <div class="progress-bar mr-2" style="width: {{ $porcentaje }}%">
+                                                </div>
                                                 <span>{{ number_format($porcentaje, 0) }}%</span>
                                             </div>
                                             <p>{{ $proyecto->total_fase }} proyectos</p>
                                         @elseif($proyecto->fase == 'Fase Despachos')
                                             <p><strong>Fase Despachos</strong></p>
-                                            <div class="progress-bar" style="width: {{ $porcentaje }}%">
+                                            <div class="d-flex">
+                                                <div class="progress-bar mr-2" style="width: {{ $porcentaje }}%">
+                                                </div>
                                                 <span>{{ number_format($porcentaje, 0) }}%</span>
                                             </div>
                                             <p>{{ $proyecto->total_fase }} proyectos</p>
                                         @elseif($proyecto->fase == 'Fase Postventa')
                                             <p><strong>Fase Postventa</strong></p>
-                                            <div class="progress-bar" style="width: {{ $porcentaje }}%">
+                                            <div class="d-flex">
+                                                <div class="progress-bar mr-2" style="width: {{ $porcentaje }}%">
+                                                </div>
                                                 <span>{{ number_format($porcentaje, 0) }}%</span>
                                             </div>
                                             <p>{{ $proyecto->total_fase }} proyectos</p>
