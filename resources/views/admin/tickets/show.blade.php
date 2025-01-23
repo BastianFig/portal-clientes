1d34949b79ec0e2d9e3530aebf2bb8ece17adba3
Jhonatan Vergara <j.vergaracajas@gmail.com>
2025-01-23 14:40:19 -0300

    fx

--- resources/views/admin/tickets/show.blade.php
+++ resources/views/admin/tickets/show.blade.php
@@ -23,7 +23,7 @@
                                         value="42">
                                     <input type="hidden" id="ticket_id" class="ticket_id" name="ticket_id"
                                         value="{{$ticket->id}}">
-                                    @if($ticket->proyecto->id_vendedor == 42)
+                                    @if($ticket->vendedor_id == 42)
                                         <input type="submit" class="btn btn-warning ml-5 mr-5" href="#"
                                             style="border-radius:10px; color:black;" value="Asignar a Post Venta" disabled>
                                     @else
